c98007cd6905471d93f1ea0b744adc2b4f7027bd
Michael Quigley <michael@quigley.com>
2021-08-27 15:45:49 -0400

    ChannelAdapter (#15)

--- adapter.go
+++ adapter.go
@@ -1,4 +1,26 @@
 package datamesh
 
-type ChannelAdapter struct{
+import (
+	"github.com/openziti-incubator/datamesh/channel"
+	"io"
+)
+
+type ChannelAdapter struct {
+	ch channel.Channel
+}
+
+func NewChannelAdapter(ch channel.Channel) *ChannelAdapter {
+	return &ChannelAdapter{ch}
+}
+
+func (ca *ChannelAdapter) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+func (ca *ChannelAdapter) Write(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+func (ca *ChannelAdapter) Close() error {
+	return nil
 }
